8fae75a582c50bf88cfbcaf9040dadcf0cdf833a
Collins Wachira Maina <97540825+Collinswama@users.noreply.github.com>
2022-01-14 09:04:37 +0300

    Create lear.py

---
+++ lear.py
@@ -0,0 +1,13 @@
+gene1=brac1
+
+gene2=brac2
+
+mut_one="malignancy of the mammary glands"
+
+mut_two="mutations of the ovarin cells"
+
+lead="p53/toumour suppressor fault"
+print(gene1) 
+print(gene2)
+print(mut_one)
+print(mut_two)
